397a20ed03abb49bebdff94454e4f4cbd70ee70d
Lindson França <l1nds0n@hotmail.com>
2021-05-05 17:20:54 -0300

    Database connection setup

--- Unit1.dfm
+++ Unit1.dfm
@@ -26,4 +26,13 @@ object DataModule2: TDataModule2
     Left = 96
     Top = 56
   end
+  object con1: TFDConnection
+    Params.Strings = (
+      'Database=C:\Users\Lindson\Documents\FirebirdSQL\EXEMPLO FB30.FDB'
+      'User_Name=SYSDBA'
+      'Password=masterkey'
+      'DriverID=FB')
+    Left = 192
+    Top = 56
+  end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -4,11 +4,15 @@ interface
 
 uses
   System.SysUtils, System.Classes, uDWDatamodule, uDWAbout, uRESTDWServerEvents,
-  uDWJSONObject, System.Json;
+  uDWJSONObject, System.Json, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.FB,
+  FireDAC.Phys.FBDef, FireDAC.VCLUI.Wait, Data.DB, FireDAC.Comp.Client;
 
 type
   TDataModule2 = class(TServerMethodDataModule)
     DWServerEvents1: TDWServerEvents;
+    con1: TFDConnection;
     procedure DWServerEvents1EventstesteReplyEvent(var Params: TDWParams; var Result: string);
   private
     { Private declarations }
